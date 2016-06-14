b173eb6b1b04d0ebf55ea6076ab69e52a9abd8cf
mbovan <bovanmilos@gmail.com>
2016-06-14 14:35:48 +0200

    Fixed Mailhandler kernel test.

--- tests/src/Kernel/MailhandlerNodeTest.php
+++ tests/src/Kernel/MailhandlerNodeTest.php
@@ -6,6 +6,8 @@ use Drupal\inmail\Entity\DelivererConfig;
 use Drupal\inmail\Entity\HandlerConfig;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\node\Entity\NodeType;
+use Drupal\user\Entity\Role;
+use Drupal\user\Entity\User;
 
 /**
  * Tests the Node handler plugin.
@@ -37,7 +39,8 @@ class MailhandlerNodeTest extends KernelTestBase {
     $this->installEntitySchema('user');
     $this->installEntitySchema('node');
     $this->installEntitySchema('inmail_handler');
-    $this->installConfig(['inmail', 'mailhandler_d8', 'node']);
+    $this->installSchema('system', ['sequences']);
+    $this->installConfig(['inmail', 'mailhandler_d8', 'node', 'user']);
 
     // Create a sample node type.
     $this->contentType = NodeType::create([
@@ -46,19 +49,37 @@ class MailhandlerNodeTest extends KernelTestBase {
     ]);
     $this->contentType->save();
     node_add_body_field($this->contentType);
+
+    // Create a new role.
+    $role = Role::create([
+      'id' => 'mailhandler',
+      'label' => 'Mailhandler',
+    ]);
+    $role->grantPermission('create nodes via email');
+    $role->save();
+
+    // Create a new user with "Mailhandler" role.
+    /** @var \Drupal\user\Entity\User $user */
+    $user = User::create([
+      'mail' => 'milos@example.com',
+      'name' => 'Milos',
+    ]);
+    $user->addRole($role->id());
+    $user->save();
+    $this->user = $user;
+
+    $this->processor = \Drupal::service('inmail.processor');
+    $this->parser = \Drupal::service('inmail.mime_parser');
+    $this->deliverer = DelivererConfig::create(['id' => 'test']);
   }
 
   /**
    * Tests features of Mailhandler Node plugin.
    */
   public function testMailhandlerNodePlugin() {
-    /** @var \Drupal\inmail\MessageProcessor $processor */
-    $processor = \Drupal::service('inmail.processor');
-    $mail_path = drupal_get_path('module', 'mailhandler_d8') . '/tests/eml/node.eml';
-    $node_mail = file_get_contents(DRUPAL_ROOT . '/' . $mail_path);
-    /** @var \Drupal\inmail\MIME\ParserInterface $parser */
-    $parser = \Drupal::service('inmail.mime_parser');
-    $node_mail_parsed = $parser->parseMessage($node_mail);
+    $raw_node_mail = $this->getFileContent('node.eml');
+    /** @var \Drupal\inmail\MIME\MessageInterface $node_mail */
+    $node_mail = $this->parser->parseMessage($raw_node_mail);
 
     // Assert default handler configuration.
     /** @var \Drupal\inmail\Entity\HandlerConfig $handler_config */
@@ -69,7 +90,7 @@ class MailhandlerNodeTest extends KernelTestBase {
     $handler_config->setConfiguration(['content_type' => $this->contentType->id()])->save();
 
     // Process the mail.
-    $processor->process($node_mail, DelivererConfig::create(['id' => 'test']));
+    $this->processor->process($raw_node_mail, $this->deliverer);
 
     // Assert there is a new node created.
     $nodes = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple();
@@ -77,11 +98,63 @@ class MailhandlerNodeTest extends KernelTestBase {
     $node = reset($nodes);
 
     // Assert the node field values.
-    $this->assertEquals($node_mail_parsed->getSubject(), $node->getTitle());
-    $this->assertEquals($node_mail_parsed->getDecodedBody(), $node->get('body')->value);
+    $this->assertEquals($node_mail->getSubject(), $node->getTitle());
+    $this->assertEquals($node_mail->getBody(), $node->get('body')->value);
     $this->assertEquals('full_html', $node->get('body')->format);
-    $this->assertEquals(0, $node->getOwnerId());
+    $this->assertEquals($this->user->id(), $node->getOwnerId());
     $this->assertEquals($this->contentType->id(), $node->getType());
     $this->assertEquals(NODE_PUBLISHED, $node->get('status')->value);
   }
+
+  /**
+   * Tests signed mail messages.
+   */
+  public function testSignedMails() {
+    $raw_signed_mail = $this->getFileContent('signed/inline.eml');
+    /** @var \Drupal\inmail\MIME\MessageInterface $signed_mail */
+    $signed_mail = $this->parser->parseMessage($raw_signed_mail);
+
+    // Add a public key to the user.
+    $this->user->set('mailhandler_gpg_key', ['public_key' => $this->getFileContent('keys/example.key')]);
+    $this->user->save();
+
+    // Update the handler configuration.
+    /** @var \Drupal\inmail\Entity\HandlerConfig $handler_config */
+    $handler_config = HandlerConfig::load('mailhandler_node');
+    $handler_config->setConfiguration(['content_type' => $this->contentType->id()])->save();
+
+    // Process the mail.
+    $this->processor->process($raw_signed_mail, $this->deliverer);
+
+    // Assert there is a new node created.
+    $nodes = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple();
+    /** @var \Drupal\node\NodeInterface $node */
+    $node = reset($nodes);
+
+    // @todo: Uncomment after enabling GnuGP extension in tests?
+    // Assert the node field values.
+    /*$this->assertEquals($signed_mail->getSubject(), $node->getTitle());
+    $this->assertEquals('Hello world!', $node->get('body')->value);
+    $this->assertEquals('full_html', $node->get('body')->format);
+    $this->assertEquals($this->user->id(), $node->getOwnerId());
+    $this->assertEquals($this->contentType->id(), $node->getType());
+    $this->assertEquals(NODE_PUBLISHED, $node->get('status')->value);*/
+  }
+
+  /**
+   * Returns the content of a requested file.
+   *
+   * See \Drupal\Tests\inmail\Kernel\ModeratorForwardTest.
+   *
+   * @param string $filename
+   *   The name of the file.
+   *
+   * @return string
+   *   The content of the file.
+   */
+  public function getFileContent($filename) {
+    $path = drupal_get_path('module', 'mailhandler_d8') . '/tests/eml/' . $filename;
+    return file_get_contents(DRUPAL_ROOT . '/' . $path);
+  }
+
 }
