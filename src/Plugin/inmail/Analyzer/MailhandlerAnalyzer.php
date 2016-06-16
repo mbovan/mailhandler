cf8ab702e2b8d8b601688dc2a387c257dcf18468
mbovan <mbovan@3149657.no-reply.drupal.org>
2016-06-16 20:41:04 +0200

    Issue #2747293 by mbovan: Update Mailhandler analyzer with body analysis

--- src/Plugin/inmail/Analyzer/MailhandlerAnalyzer.php
+++ src/Plugin/inmail/Analyzer/MailhandlerAnalyzer.php
@@ -3,6 +3,7 @@
 namespace Drupal\mailhandler_d8\Plugin\inmail\Analyzer;
 
 use Drupal\inmail\MIME\MessageInterface;
+use Drupal\inmail\MIME\MultipartEntity;
 use Drupal\inmail\MIME\MultipartMessage;
 use Drupal\inmail\Plugin\inmail\Analyzer\AnalyzerBase;
 use Drupal\inmail\ProcessorResultInterface;
@@ -41,7 +42,8 @@ class MailhandlerAnalyzer extends AnalyzerBase {
     }
 
     // Populate general properties.
-    $this->findSender($message, $result);
+    $this->findBody($message, $result, $context);
+    $this->findSender($message, $result, $context);
     $this->findUser($result);
   }
 
@@ -74,8 +76,11 @@ class MailhandlerAnalyzer extends AnalyzerBase {
 
             // In order to find a signed text part of the message, we need to
             // skip the signature.
-            $signed_text_index = reset(array_diff(array_keys($message->getParts()), [$index]));
+            $message_parts = array_diff(array_keys($message->getParts()), [$index]);
+            $signed_text_index = reset($message_parts);
             $signed_text_part = $message->getPart($signed_text_index);
+            // Add index of the signed message part to the context.
+            $context['signed_text_index'] = $signed_text_index;
             // Include headers into the signed text.
             $context['signed_text'] = preg_replace('~\R~u', "\r\n", $signed_text_part->toString());
 
@@ -108,21 +113,91 @@ class MailhandlerAnalyzer extends AnalyzerBase {
   }
 
   /**
+   * Analyzes the body part of the given message.
+   *
+   * @param \Drupal\inmail\MIME\MessageInterface $message
+   *   The mail message.
+   * @param \Drupal\mailhandler_d8\MailhandlerAnalyzerResultInterface $result
+   *   The analyzer result.
+   * @param array $context
+   *   The array with context data.
+   *
+   * @return string
+   *   The analyzed message body.
+   */
+  protected function findBody(MessageInterface $message, MailhandlerAnalyzerResultInterface $result, array &$context) {
+    // By default, use original message body.
+    $body = $message->getBody();
+
+    if ($context) {
+      // Extract body from PGP/MIME messages.
+      if ($context['pgp_type'] == 'mime') {
+        /** @var \Drupal\inmail\MIME\MultipartMessage $message */
+        /** @var \Drupal\inmail\MIME\MultipartEntity $signed_message_part */
+        $signed_message_part = $message->getPart($context['signed_text_index']);
+        $body = '';
+        foreach ($signed_message_part->getParts() as $part) {
+          // Extract the body from HTML messages.
+          if ($part instanceof MultipartEntity) {
+            foreach ($part->getParts() as $message_part) {
+              if ($message_part->getContentType()['subtype'] == 'html') {
+                $body .= $message_part->getBody();
+              }
+            }
+          }
+          else {
+            $body .= $part->getBody();
+          }
+        }
+      }
+      // Support for clear-text signed messages.
+      if ($context['pgp_type'] == 'inline') {
+        // Since the message was already checked for valid PGP signature, we
+        // can use the analyzed result instead of the raw message body.
+        // See \Drupal\mailhandler_d8\Plugin\inmail\Analyzer\MailhandlerAnalyzer::isSigned
+        $pgp_parts = explode("-----BEGIN PGP SIGNATURE-----\r\n", $context['signed_text']);
+        // Get the message digest by following RFC 4880 recommendations.
+        // See https://tools.ietf.org/html/rfc4880#section-7.
+        // Remove PGP message header.
+        $body = preg_replace("/^.*\n/", "", reset($pgp_parts));
+        // In case there is a "Hash" header, remove it.
+        $body = preg_replace("/Hash:.*\n/i", "", $body);
+        // Remove empty line before the message digest.
+        $body = preg_replace("/^.*\n/", "", $body);
+      }
+    }
+
+    // @todo: Support analysis of unsigned Multipart messages.
+    $result->setBody($body);
+    return $body;
+  }
+
+    /**
    * Finds the sender from given mail message.
    *
    * @param \Drupal\inmail\MIME\MessageInterface $message
    *   The mail message.
    * @param \Drupal\mailhandler_d8\MailhandlerAnalyzerResultInterface $result
    *   The analyzer result.
+   * @param array $context
+   *   The array with context data.
    *
    * @return string|null
    *   The sender of the mail message or null if not found.
    */
-  protected function findSender(MessageInterface $message, MailhandlerAnalyzerResultInterface $result) {
+  protected function findSender(MessageInterface $message, MailhandlerAnalyzerResultInterface $result, array &$context) {
     $sender = NULL;
     $matches = [];
-    preg_match('/[^@<\s]+@[^@\s>]+/', $message->getFrom(), $matches);
+    $from = $message->getFrom();
 
+    // Use signed headers to extract "from" address for PGP/MIME messages.
+    if (isset($context['pgp_type']) && $context['pgp_type'] == 'mime') {
+      /** @var \Drupal\inmail\MIME\MultipartEntity $message */
+      $signed_text_part = $message->getPart($context['signed_text_index']);
+      $from = $signed_text_part->getHeader()->getFieldBody('From') ?: $message->getFrom();
+    }
+
+    preg_match('/[^@<\s]+@[^@\s>]+/', $from, $matches);
     if (!empty($matches)) {
       $sender = reset($matches);
     }
