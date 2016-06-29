eb3ca4c0eed7524e7aa10fbe103b7cb889f5b2e9
mbovan <mbovan@3149657.no-reply.drupal.org>
2016-06-29 19:54:04 +0200

    Issue #2757999 by mbovan: Update Footer analyzer

--- src/Plugin/inmail/Analyzer/FooterAnalyzer.php
+++ src/Plugin/inmail/Analyzer/FooterAnalyzer.php
@@ -7,8 +7,6 @@ use Drupal\inmail\Plugin\inmail\Analyzer\AnalyzerBase;
 use Drupal\inmail\ProcessorResultInterface;
 use Drupal\mailhandler_d8\MailhandlerAnalyzerResult;
 use Drupal\mailhandler_d8\MailhandlerAnalyzerResultInterface;
-use Drupal\mailhandler_d8\MailhandlerAnalyzerResultSigned;
-use Drupal\mailhandler_d8\Plugin\inmail\Handler\MailhandlerNode;
 
 /**
  * A message footer analyzer.
@@ -26,31 +24,23 @@ class FooterAnalyzer extends AnalyzerBase {
    * {@inheritdoc}
    */
   public function analyze(MessageInterface $message, ProcessorResultInterface $processor_result) {
-    // In order to work with signed messages, get the result of
-    // Mailhandler analyzer plugin.
-    if (MailhandlerNode::isMessageSigned($processor_result)) {
-      /** @var \Drupal\mailhandler_d8\MailhandlerAnalyzerResultSigned $result */
-      $result = $processor_result->ensureAnalyzerResult(MailhandlerAnalyzerResultSigned::TOPIC, MailhandlerAnalyzerResultSigned::createFactory());
-    }
-    else {
-      $result = $processor_result->ensureAnalyzerResult(MailhandlerAnalyzerResult::TOPIC, MailhandlerAnalyzerResult::createFactory());
-    }
+    /** @var \Drupal\mailhandler_d8\MailhandlerAnalyzerResultInterface $result */
+    $result = $processor_result->ensureAnalyzerResult(MailhandlerAnalyzerResult::TOPIC, MailhandlerAnalyzerResult::createFactory());
 
-    $this->findFooter($result);
+    $this->findFooter($message, $result);
   }
 
   /**
    * Finds and returns the message footer.
    *
+   * @param \Drupal\inmail\MIME\MessageInterface $message
+   *   A mail message to be analyzed.
    * @param \Drupal\mailhandler_d8\MailhandlerAnalyzerResultInterface $result
    *   The analyzer result.
-   *
-   * @return string|null
-   *   The message footer or null if not found.
    */
-  protected function findFooter(MailhandlerAnalyzerResultInterface $result) {
+  protected function findFooter(MessageInterface $message, MailhandlerAnalyzerResultInterface $result) {
     // Get a message body.
-    $body = $result->getBody();
+    $body = $result->getBody() ?: $message->getBody();
     $footer = NULL;
 
     // Per https://tools.ietf.org/html/rfc3676#section-4.3, footer/signature is
@@ -64,18 +54,17 @@ class FooterAnalyzer extends AnalyzerBase {
       // Update the analyzed body without footer.
       $footer_key = count($body_match) - 1;
       unset($body_match[$footer_key]);
-      $body = nl2br(implode("\n-- \n", $body_match));
+      $body = implode("\n-- \n", $body_match);
       $result->setBody($body);
     }
     // Match "On {day}, {month} {date}, {year} at {hour}:{minute} {AM|PM}".
     elseif (preg_match('/On [A-Za-z]{3}, [A-Za-z]{3} [0-9]{1,2}, 20[0-9]{2} at [0-9]{1,2}:[0-9]{2} (AM|PM).+/', $body, $matches)) {
       $footer_line = reset($matches);
       $footer = strstr($body, $footer_line);
-      $result->setBody(nl2br(strstr($body, $footer_line, TRUE)));
+      $result->setBody(strstr($body, $footer_line, TRUE));
     }
 
     $result->setFooter($footer);
-    return $footer;
   }
 
 }
