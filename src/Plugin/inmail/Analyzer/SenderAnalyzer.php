0ab3ab3992503f9335d2765f3c337693b94dc3c1
mbovan <mbovan@3149657.no-reply.drupal.org>
2016-06-29 20:17:06 +0200

    Issue #2758025 by mbovan: Implement Sender analyzer

---
+++ src/Plugin/inmail/Analyzer/SenderAnalyzer.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\mailhandler_d8\Plugin\inmail\Analyzer;
+
+use Drupal\inmail\MIME\MessageInterface;
+use Drupal\inmail\Plugin\inmail\Analyzer\AnalyzerBase;
+use Drupal\inmail\ProcessorResultInterface;
+use Drupal\mailhandler_d8\MailhandlerAnalyzerResult;
+use Drupal\mailhandler_d8\MailhandlerAnalyzerResultInterface;
+
+/**
+ * Finds the sender based on "From" mail header field.
+ *
+ * As this option is not entirely safe, it is disabled by default.
+ *
+ * @ingroup analyzer
+ *
+ * @Analyzer(
+ *   id = "sender",
+ *   label = @Translation("User Sender Analyzer")
+ * )
+ */
+class SenderAnalyzer extends AnalyzerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function analyze(MessageInterface $message, ProcessorResultInterface $processor_result) {
+    /** @var \Drupal\mailhandler_d8\MailhandlerAnalyzerResultInterface $result */
+    $result = $processor_result->ensureAnalyzerResult(MailhandlerAnalyzerResult::TOPIC, MailhandlerAnalyzerResult::createFactory());
+
+    $this->findSender($message, $result);
+  }
+
+  /**
+   * Finds the message sender.
+   *
+   * @param \Drupal\inmail\MIME\MessageInterface $message
+   *   The mail message.
+   * @param \Drupal\mailhandler_d8\MailhandlerAnalyzerResultInterface $result
+   *   The analyzed message result.
+   */
+  protected function findSender(MessageInterface $message, MailhandlerAnalyzerResultInterface $result) {
+    $sender = NULL;
+    $user = NULL;
+    $matches = [];
+    $from = $message->getFrom();
+
+    preg_match('/[^@<\s]+@[^@\s>]+/', $from, $matches);
+    if (!empty($matches)) {
+      $sender = reset($matches);
+    }
+
+    $matched_users = \Drupal::entityTypeManager()->getStorage('user')->loadByProperties(['mail' => $sender]);
+    if (!empty($matched_users)) {
+      $user = reset($matched_users);
+    }
+
+    // Do not override a sender/user in case there is already one set.
+    if (!$result->getSender()) {
+      $result->setSender($sender);
+    }
+    if ($user && !$result->isUserAuthenticated()) {
+      $result->setUser($user);
+    }
+  }
+
+}
