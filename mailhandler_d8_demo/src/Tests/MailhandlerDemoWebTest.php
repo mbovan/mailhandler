a83223651d005ff74d3fd42d7029cebeba28f0f6
mbovan <mbovan@3149657.no-reply.drupal.org>
2016-07-09 19:39:55 +0200

    Issue #2763027 by mbovan: Extend Inmail "Paste form" to support Mailhandler samples

---
+++ mailhandler_d8_demo/src/Tests/MailhandlerDemoWebTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\mailhandler_d8_demo\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Tests the UI of Mailhandler Demo.
+ *
+ * @group mailhandler_d8
+ */
+class MailhandlerDemoWebTest extends WebTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'mailhandler_d8_demo',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    // Create a test user and log in.
+    $this->user = $this->drupalCreateUser([
+      'access administration pages',
+      'administer inmail',
+    ]);
+    $this->drupalLogin($this->user);
+  }
+
+  /**
+   * Tests user interface of Mailhandler Demo.
+   */
+  protected function testMailhandlerDemoUi() {
+    $this->drupalGet('admin/config/system/inmail/paste');
+    $sample_messages = (array) $this->xpath('//*[@id="edit-example"]')[0]->option;
+
+    // Assert sample messages are available in the list of sample messages.
+    $this->assertTrue(in_array('PGP_Signed_MIME.eml', $sample_messages), 'PGP Signed MIME message is in the last of sample messages.');
+    $this->assertTrue(in_array('PGP_Signed_MIME_HTML.eml', $sample_messages), 'PGP Signed MIME HTML message is in the last of sample messages.');
+    $this->assertTrue(in_array('PGP_Signed_Inline.eml', $sample_messages), 'PGP Signed Inline message is in the last of sample messages.');
+  }
+
+}
